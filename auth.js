6465cfdc4896b069161f402a70d9819092b3e3c7
phamhuynhtantai0206-ship-it <phamhuynhtantai0206@gmail.com>
2026-09-16 19:10:48 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function login(username, password) {
     // Check administrator login
-    if (username === "admin" && password === "123") {
+    if (username === "admin" && password === "9999") {
         return true;
     }
 
